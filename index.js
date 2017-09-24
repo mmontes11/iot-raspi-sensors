9b015f166cf59ec86e961fc6e2a4c88124999422
mmontes11 <martin11lrx@gmail.com>
2017-09-24 19:37:29 +0000

    Added some crap for testint purposes

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { LedHandler } from './src/util/ledHandler';
 
 const ledHandler = new LedHandler();
-ledHandler.blinkForSuccess();
+if (Math.random() >= 0.5) {
+	ledHandler.blinkForSuccess();
+} else {
+	ledHandler.blinkForError();
+}
