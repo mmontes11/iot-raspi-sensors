8f986ff039269e4f6e06383e32a18990e86741cd
mmontes11 <martin11lrx@gmail.com>
2017-09-25 15:40:47 +0000

    More efficient way of transpiling

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,7 +7,7 @@ import runSequence from 'run-sequence';
 const plugins = gulpLoadPlugins();
 
 const paths = {
-    js: ['./**/*.js', '!dist/**', '!node_modules/**'],
+    js: ['./**/*.js', '!dist/**', '!node_modules/**', "!lib/iot_client/**"],
     nonJs: ['./package.json']
 };
 
