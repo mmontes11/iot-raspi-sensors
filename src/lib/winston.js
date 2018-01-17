bc1ced7db010a96e93ef32d510e2751ead4ec740
mmontes11 <martin11lrx@gmail.com>
2018-01-17 20:54:56 +0100

    Winston logger properly configured

--- src/lib/winston.js
+++ src/lib/winston.js
@@ -1,17 +1,19 @@
 import winston from 'winston';
 
-winston.remove(winston.transports.Console);
-
-winston.add(winston.transports.Console, {
-    timestamp: false,
-    json: false,
-    colorize: true
+const logger = new (winston.Logger)({
+    level: 'info',
+    transports: [
+        new winston.transports.Console ({
+            timestamp: false,
+            json: false,
+            colorize: true
+        }),
+        new winston.transports.File({
+            timestamp: true,
+            json: false,
+            colorize: true,
+            filename: 'log_iot_raspi.log'
+        })
+    ]
 });
-winston.add(winston.transports.File, {
-    timestamp: true,
-    json: false,
-    colorize: true,
-    filename: 'log_iot_raspi.log'
-});
-
 export default winston;
