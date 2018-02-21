cc8f87f5cf1d677ba44aff024766951845225c96
mmontes11 <martin11lrx@gmail.com>
2018-02-21 16:47:13 +0100

    Adapted env variables for cron

--- scripts/start_node.sh
+++ scripts/start_node.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 
 export NODE_ENV='production';
+export IOT_SERVER='http://localhost:8000';
+export IOT_USERNAME='admin';
+export IOT_PASSWORD='aA12345678&';
+export IOT_DEBUG='true';
+export IOT_RASPI_LONGITUDE=-8.40;
+export IOT_RASPI_LATITUDE=43.40;
+export IOT_RASPI_SUCCESS_LED_GPIO=20;
+export IOT_RASPI_ERROR_LED_GPIO=21;
+export IOT_RASPI_LED_BLINK_DURATION_IN_MS=100;
+export IOT_RASPI_LED_BLINK_TOTAL_IN_MS=5000;
+export IOT_RASPI_DHT_TYPE=11;
+export IOT_RASPI_DHT_GPIO=24;
+
 /usr/local/bin/node /usr/src/iot-raspi/index.js
