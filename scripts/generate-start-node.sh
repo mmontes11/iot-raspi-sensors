fc8f98c1a2729aaf56aa3367db07d7b712057c32
mmontes11 <martin11lrx@gmail.com>
2021-12-25 19:46:20 +0000

    Minor fix in generate-start-node.sh

--- scripts/generate-start-node.sh
+++ scripts/generate-start-node.sh
@@ -7,7 +7,7 @@ SCRIPT=$WORKDIR/start-node.sh
 
 shebang="#!/usr/bin/env bash";
 node_env="export NODE_ENV=$NODE_ENV;\n"
-iot_env=$(printenv | sed 's/^\(.*\)$/export \1/g' | grep -E '^export IOT' | awk '{ printf("%s;\\n", $0) }')
+iot_env=$(printenv | sed 's/^\(.*\)$/export \1/g' | awk '{ printf("%s;\\n", $0) }')
 env=$node_env$iot_env;
 run="$NODE_PATH/node $WORKDIR/index.js";
 
